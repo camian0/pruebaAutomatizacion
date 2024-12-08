31e5286c392c3e4e387aa930cc14c38ee04fed09
camian <camian6@outlook.com>
2024-12-07 19:21:53 -0500

    some configurations were changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"], // Define las extensiones a analizar
+    languageOptions: {
+      globals: globals.browser, // Habilita variables globales del navegador
+    },
+    rules: {
+      "no-console": "warn", // Ejemplo: advertencia para uso de console.log
+      "no-unused-vars": "error", // Error si hay variables no usadas
+    },
+  },
 ];
